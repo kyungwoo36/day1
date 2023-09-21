177d1853ec89193f7773eedb7885d8ae546c65b6
5671218@stu.kmu.ac.kr <5671218@stu.kmu.ac.kr>
2023-09-21 12:44:40 +0900

    20230921Thread실습

--- day1/Tree.c
+++ day1/Tree.c
@@ -1,103 +1,86 @@
 #include<stdio.h>
-#include<stdlib.h>
-
+#define TRUE 1
+#define FALSE 0
 
 typedef struct TreeNode {
 	int data;
 	struct TreeNode* left, * right;
+	int is_thread; // 스레드이면 1, 스레드가 아니면 0
 }TreeNode;
 
-	TreeNode n1 = {4, NULL, NULL};
-	TreeNode n2 = {5, NULL, NULL};
-	TreeNode n3 = {3, &n1, &n2};
-	TreeNode n4 = {6, NULL, NULL};
-	TreeNode n5 = {2, &n3, &n4};
+TreeNode n1 = { 4, NULL, NULL, 0 };
+TreeNode n2 = { 5, NULL, NULL ,0 };
+TreeNode n3 = { 3, &n1, &n2 ,0 };
+TreeNode n4 = { 6, NULL, NULL,0 };
+TreeNode n5 = { 2,&n3, &n4 ,0 };
+TreeNode n6 = { 8, NULL, NULL,0 };
+TreeNode n7 = { 10, NULL, NULL,0 };
+TreeNode n8 = { 11, NULL, NULL ,0 };
+TreeNode n9 = { 9, &n7, &n8 ,0 };
+TreeNode n10 = { 7, &n6, &n9 ,0 };
+TreeNode n11 = { 1, &n5, &n10 ,0 };
+TreeNode* root = &n11;
 
-	TreeNode n6 = {8, NULL, NULL};
-	TreeNode n7 = {10, NULL, NULL};
-	TreeNode n8 = {11, NULL, NULL};
-	TreeNode n9 = {9, &n7, &n8};
-	TreeNode n10 = {7, &n6, &n9};
-	TreeNode n11 = {1, &n5, &n10 };
-	TreeNode* root = &n11;
-
-void preorder(TreeNode* root) {
-	if (root != NULL) {
-		printf("%d ", root->data);
-		preorder(root->left);
-		preorder(root->right);
-	}
+TreeNode* find_successor(TreeNode* p)
+{
+	TreeNode* q = p->right;
+	if (q == NULL || p->is_thread == TRUE)
+		return q;
+	while (q->left != NULL)
+		q = q->left;
+	return q;
 }
 
-void inorder(TreeNode* root) {
-	if (root != NULL) {
-		inorder(root->left);
-		printf("%d ", root->data);
-		inorder(root->right);
-	}
+void thread_inorder(TreeNode* t)
+{
+	TreeNode* q;
+	q = t;
+	while (q->left)
+		q = q->left;
+	do {
+		printf("%d ", q->data);
+		q = find_successor(q);
+	} while (q);
 }
 
-void postorder(TreeNode* root) {
-	if (root != NULL) {
-		postorder(root->left);
-		postorder(root->right);
-		printf("%d ", root->data);
+TreeNode* parent(TreeNode* child)
+{
+	TreeNode* q;
+	q = child;
+	while (q->left)
+		q = q->left;
+	if (q->right->left == q) {
+		q = q->right;
+		printf("%d\n", q->data);
 	}
-}
-
-int ArrayTree[16] = { 0,1,2,7,3,6,8,9,4,5,0,0,0,0,10,11 };
-
-void Array_preorder(int i) {
-	if (i && ArrayTree[i] != 0) {
-		printf("%d ", ArrayTree[i]);
-		Array_preorder(2 * i);
-		Array_preorder(2 * i + 1);
-	}
-}
-
-void Array_inorder(int i) {
-	if (i && ArrayTree[i] != 0) {
-		Array_inorder(2 * i);
-		printf("%d ", ArrayTree[i]);
-		Array_inorder(2 * i + 1);
-	}
-}
-
-void Array_postorder(int i) {
-	if (i && ArrayTree[i] != 0) {
-		Array_postorder(2 * i);
-		Array_postorder(2 * i + 1);
-		printf("%d ", ArrayTree[i]);
+	else {
+		q = q->right->left;
+		printf("%d\n", q->data);
 	}
 }
 
 int main(void)
 {
-	printf("Linked Tree\n");
-	printf("1. 전위 순회\n");
-		preorder(root);
-	printf("\n");
+	n1.right = &n3;
+	n1.is_thread = 1;
+	n2.right = &n5;
+	n2.is_thread = 1;
+	n4.right = &n11;
+	n4.is_thread = 1;
+	n6.right = &n10;
+	n6.is_thread = 1;
+	n7.right = &n9;
+	n7.is_thread = 1;
 
-	printf("2. 중위 순회\n");
-		inorder(root);
-	printf("\n");
-
-	printf("3. 후위 순회\n");
-		postorder(root);
+	printf("1.중위 순회 결과\n");
+	thread_inorder(root);
 	printf("\n\n");
-
-	printf("Array Tree\n");
-	printf("1. 전위 순회\n");
-	Array_preorder(1);
-	printf("\n");
-
-	printf("2. 중위 순회\n");
-	Array_inorder(1);
-	printf("\n");
-
-	printf("3. 후위 순회\n");
-	Array_postorder(1);
-	printf("\n");
+	printf("2. Node 4의 부모: ");
+	parent(&n1);
+	printf("3. Node 5의 부모: ");
+	parent(&n2);
+	printf("4. Node 6의 부모: ");
+	parent(&n4);
 
 	return 0;
 }
