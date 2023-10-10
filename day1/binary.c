f6b5b95e7356c3ba8f9e8bb40eab5175508f2d70
5671218@stu.kmu.ac.kr <5671218@stu.kmu.ac.kr>
2023-10-10 09:19:33 +0900

    조금 더 수정

--- day1/binary.c
+++ day1/binary.c
@@ -1,61 +1,60 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-typedef struct TreeNode{
-	int key;
-	struct TreeNode* left, * right;
-}TreeNode;
-
-TreeNode n1 = { 25,NULL,NULL };
-TreeNode n2 = { 16,NULL ,&n1 };
-TreeNode n3 = { 42,NULL ,NULL };
-TreeNode n4 = { 46,&n3,NULL };
-TreeNode n5 = { 55,NULL ,NULL };
-TreeNode n6 = { 53, &n4,&n5 };
-TreeNode n7 = { 41,&n2, &n6 };
-
-TreeNode n8 = { 62,NULL ,NULL };
-TreeNode n9 = { 64,NULL ,NULL };
-TreeNode n10 = { 63,&n8,&n9 };
-TreeNode n11 = { 70,NULL ,NULL };
-TreeNode n12 = { 65,&n10,&n11 };
-TreeNode n13 = { 74,&n12,NULL };
-TreeNode n14 = { 60,&n7,&n13 };
-TreeNode* root = &n14;
-
-TreeNode* search(TreeNode* node, int key)
-{
-	int i;
-	while (node != NULL) {
-		if (key == node->key) {
-			i++;
-			return node;
-		}
-		else if (key < node->key)
-			node = node->left;
-		else
-			node = node->right;
-	}
-	return NULL; 	// 탐색에 실패했을 경우 NULL 반환
-}
-
-
-int main(void)
-{
-	//char input;
-	//switch (input)
-	//{
-	//case 's':
-	//case 'i':
-	//case 'd':
-	//case 't':
-	//case 'I':
-	//case 'D':
-	//default:
-	//	break;
-	//}
-
-	search(root, 25);
-
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//typedef struct TreeNode{
+//	int key;
+//	struct TreeNode* left, * right;
+//}TreeNode;
+//
+//TreeNode n1 = { 25,NULL,NULL };
+//TreeNode n2 = { 16,NULL ,&n1 };
+//TreeNode n3 = { 42,NULL ,NULL };
+//TreeNode n4 = { 46,&n3,NULL };
+//TreeNode n5 = { 55,NULL ,NULL };
+//TreeNode n6 = { 53, &n4,&n5 };
+//TreeNode n7 = { 41,&n2, &n6 };
+//TreeNode n8 = { 62,NULL ,NULL };
+//TreeNode n9 = { 64,NULL ,NULL };
+//TreeNode n10 = { 63,&n8,&n9 };
+//TreeNode n11 = { 70,NULL ,NULL };
+//TreeNode n12 = { 65,&n10,&n11 };
+//TreeNode n13 = { 74,&n12,NULL };
+//TreeNode n14 = { 60,&n7,&n13 };
+//TreeNode* root = &n14;
+//
+//TreeNode* search(TreeNode* node, int key)
+//{
+//	int i;
+//	while (node != NULL) {
+//		if (key == node->key) {
+//			i++;
+//			return node;
+//		}
+//		else if (key < node->key)
+//			node = node->left;
+//		else
+//			node = node->right;
+//	}
+//	return NULL; 	// 탐색에 실패했을 경우 NULL 반환
+//}
+//
+//
+//int main(void)
+//{
+//	//char input;
+//	//switch (input)
+//	//{
+//	//case 's':
+//	//case 'i':
+//	//case 'd':
+//	//case 't':
+//	//case 'I':
+//	//case 'D':
+//	//default:
+//	//	break;
+//	//}
+//
+//	search(root, 25);
+//
+//	return 0;
+//}
---
+++ day1/binaryserch.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct TreeNode {
+    element key;
+    struct TreeNode* left, * right;
+}TreeNode;
+
+TreeNode* search(TreeNode* node, int key)
+{
+    while (node != NULL) {
+        if (key == node->key) return node;
+        else if (key < node->key)
+            node = node->left;
+        else
+            node = node->right;
+    }
+    return NULL;
+}
+
+TreeNode* new_node(int item)
+{
+    TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+TreeNode* insert_node(TreeNode* node, int key)
+{
+    if (node == NULL) return new_node(key);
+    if (key < node->key)
+        node->left = insert_node(node->left, key);
+    else if (key > node->key)
+        node->right = insert_node(node->right, key);
+
+    return node;
+}
+
+TreeNode* min_value_node(TreeNode* node)
+{
+    TreeNode* current = node;
+
+    while (current->left != NULL)
+        current = current->left;
+
+    return current;
+}
+
+TreeNode* delete_node(TreeNode* root, int key)
+{
+    if (root == NULL) return root;
+
+    if (key < root->key)
+        root->left = delete_node(root->left, key);
+    else if (key > root->key)
+        root->right = delete_node(root->right, key);
+    else {
+        if (root->left == NULL) {
+            TreeNode* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            TreeNode* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        TreeNode* temp = min_value_node(root->right);
+
+        root->key = temp->key;
+        root->right = delete_node(root->right, temp->key);
+    }
+    return root;
+}
+
+void inorder(TreeNode* root) {
+    if (root) {
+        inorder(root->left);
+        printf("[%d] ", root->key);
+        inorder(root->right);
+    }
+}
+
+int main(void)
+{
+    printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n");
+    printf("| s    : 검색              |\n");
+    printf("| i    : 노드 추가         |\n");
+    printf("| d    : 노드 삭제         |\n");
+    printf("| t    : 중위 순회         |\n");
+    printf("| l    : 노드 추가(반복)   |\n");
+    printf("| D    : 노드 삭제(반복)   |\n");
+    printf("| c    : 종료              |\n");
+    printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n\n");
+    TreeNode* root = NULL;
+    TreeNode* tmp = NULL;
+
+    root = insert_node(root, 60);
+    root = insert_node(root, 41);
+    root = insert_node(root, 16);
+    root = insert_node(root, 25);
+    root = insert_node(root, 53);
+    root = insert_node(root, 46);
+    root = insert_node(root, 42);
+    root = insert_node(root, 55);
+    root = insert_node(root, 74);
+    root = insert_node(root, 65);
+    root = insert_node(root, 63);
+    root = insert_node(root, 62);
+    root = insert_node(root, 64);
+    root = insert_node(root, 70);
+
+    printf("이진 탐색 트리 중위 순회 결과\n");
+    inorder(root);
+    printf("\n\n");
+    printf("메뉴 입력: ");
+    char menu;
+    scanf_s("%c", &menu);
+    int snum=0;
+    switch (menu)
+    {
+    case 's':
+        printf("검색할 값 입력: ");
+        scanf_s("%d", snum);
+        search(root, snum);
+        break;
+    case 'i':
+        break;
+    case 'd':
+        break;
+    case 't':
+        break;
+    case 'l':
+        break;
+    case 'D':
+        break;
+    case 'c':
+    default:
+        printf("옳바른 메뉴를 입력해주세요!");
+        break;
+    }
+    return 0;
+}
