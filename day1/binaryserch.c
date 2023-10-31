3389fff17fe8b904b01913167252eeabb7d72abf
5671218@stu.kmu.ac.kr <5671218@stu.kmu.ac.kr>
2023-10-31 10:50:50 +0900

    수정중

--- day1/binaryserch.c
+++ day1/binaryserch.c
@@ -1,63 +1,91 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#define MAX_ELEMENT 200
-//
-//typedef int element;
-//typedef struct {
-//    element key;
-//}Node;
-//typedef struct {
-//    element heap[MAX_ELEMENT];
-//    int heap_size;
-//}HeapType;
-//
-//HeapType* create()
-//{
-//    return (HeapType*)malloc(sizeof(HeapType));
-//}
-//
-//void insert_max_heap(HeapType* h, Node* item)
-//{
-//    int i;
-//    i = ++(h->heap_size);
-//
-//    while ((i != 1) && (item->key > h->heap[i / 2])) {
-//        h->heap[i] = h->heap[1 / 2];
-//        i /= 2;
-//    }
-//    h->heap[i] = item;
-//}
-//
-//int main(void)
-//{
-//    HeapType* heap;
-//    element heap[MAX_ELEMENT] = { 90, 89, 70, 36, 75, 63, 65, 21, 18, 15 };
-//
-//    printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n");
-//    printf("| i   : 노드 추가       |\n");
-//    printf("| d   : 노드 삭제       |\n");
-//    printf("| P   : 레벨별 출력     |\n");
-//    printf("| c   : 종료            |\n");
-//    printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n\n");
-//    printf("메뉴 입력: ");
-//    char menu;
-//    scanf_s("%c", &menu);
-//    element snum;
-//    switch (menu)
-//    {
-//    case 's':
-//        printf("검색할 값 입력: ");
-//        scanf_s("%d", &snum);
-//        insert_max_heap(heap, &snum);
-//        break;
-//    case 'd':
-//        break;
-//    case 'P':
-//        break;
-//    case 'c':
-//    default:
-//        printf("옳바른 메뉴를 입력해주세요!");
-//        break;
-//    }
-//    return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MAX_VERTICES 50
+
+typedef struct GraphType
+{
+	int n;
+	int adj_mat[MAX_VERTICES][MAX_VERTICES];
+}GraphType;
+
+int visited[MAX_VERTICES];
+
+void init(GraphType* g)
+{
+	int r, c;
+	g->n = 0;
+	for (r = 0; r < MAX_VERTICES; r++)
+		for (c = 0; c < MAX_VERTICES; c++)
+			g->adj_mat[r][c] = 0;
+}
+
+void insert_vertex(GraphType* g, int v)
+{
+	if (((g->n) + 1) > MAX_VERTICES) {
+		fprintf(stderr, "그래프: 정점의 개수 초과");
+		return;
+	}
+	g->n++;
+}
+
+void insert_edge(GraphType* g, int start, int end)
+{
+	if (start >= g->n || end >= g->n) {
+		fprintf(stderr, "그래프: 정점 번호 오류");
+		return;
+	}
+	g->adj_mat[start][end] = 1;
+	g->adj_mat[end][start] = 1;
+}
+
+// 인접 행렬로 표현된 그래프에 대한 깊이 우선 탐색
+void dfs_mat(GraphType* g, int v)
+{
+	int w;
+	visited[v] = TRUE;		// 정점 v의 방문 표시 
+	printf("정점 %d -> ", v);		// 방문한 정점 출력
+	for (w = 0; w < g->n; w++) 		// 인접 정점 탐색
+		if (g->adj_mat[v][w] && !visited[w])
+			dfs_mat(g, w);	//정점 w에서 DFS 새로 시작
+}
+
+
+int main(void)
+{
+	GraphType* g;
+	g = (GraphType*)malloc(sizeof(GraphType));
+	init(g);
+	for (int i = 0; i < 11; i++)
+		insert_vertex(g, i);
+	insert_edge(g, 0, 9);
+	insert_edge(g, 0, 6);
+	insert_edge(g, 0, 4);
+	insert_edge(g, 0, 2);
+	insert_edge(g, 0, 5);
+	insert_edge(g, 9, 8);
+	insert_edge(g, 8, 10);
+	insert_edge(g, 6, 8);
+	insert_edge(g, 6, 7);
+	insert_edge(g, 7, 10);
+	insert_edge(g, 6, 4);
+	insert_edge(g, 4, 7);
+	insert_edge(g, 2, 4);
+	insert_edge(g, 2, 3);
+	insert_edge(g, 3, 4);
+	insert_edge(g, 3, 5);
+	insert_edge(g, 5, 4);
+	insert_edge(g, 5, 1);
+	insert_edge(g, 4, 1);
+	insert_edge(g, 1, 7);
+	insert_edge(g, 1, 10);
+
+	printf("깊이 우선 탐색\n");
+	dfs_mat(g, 9);
+	printf("\n");
+	free(g);
+	return 0;
+}
+
