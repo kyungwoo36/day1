f191df477529f5cb9bad9013cc75feba16f0433a
5671218@stu.kmu.ac.kr <5671218@stu.kmu.ac.kr>
2023-11-02 10:57:40 +0900

    1차

--- day1/binaryserch.c
+++ day1/binaryserch.c
@@ -3,88 +3,136 @@
 
 #define TRUE 1
 #define FALSE 0
-#define MAX_VERTICES 50
 
-typedef struct GraphType
+#define MAX_VERTICES 100
+#define INF 1000
+
+
+int parent[MAX_VERTICES];		// 부모 노드
+// 초기화
+void set_init(int n)
 {
-	int n;
-	int adj_mat[MAX_VERTICES][MAX_VERTICES];
-}GraphType;
-
-int visited[MAX_VERTICES];
-
-void init(GraphType* g)
+	for (int i = 0; i < n; i++)
+		parent[i] = -1;
+}
+// curr가 속하는 집합을 반환한다.
+int set_find(int curr)
 {
-	int r, c;
-	g->n = 0;
-	for (r = 0; r < MAX_VERTICES; r++)
-		for (c = 0; c < MAX_VERTICES; c++)
-			g->adj_mat[r][c] = 0;
+	if (parent[curr] == -1)
+		return curr; 			// 루트 
+	while (parent[curr] != -1) curr = parent[curr];
+	return curr;
 }
 
-void insert_vertex(GraphType* g, int v)
+
+// 두개의 원소가 속한 집합을 합친다.
+void set_union(int a, int b)
 {
-	if (((g->n) + 1) > MAX_VERTICES) {
-		fprintf(stderr, "그래프: 정점의 개수 초과");
-		return;
+	int root1 = set_find(a);	// 노드 a의 루트를 찾는다. 
+	int root2 = set_find(b);	// 노드 b의 루트를 찾는다. 
+	if (root1 != root2) 	// 합한다. 
+		parent[root1] = root2;
+}
+
+struct Edge {			// 간선을 나타내는 구조체
+	int start, end, weight;
+};
+
+typedef struct GraphType {
+	int n;	// 간선의 개수
+	struct Edge edges[2 * MAX_VERTICES];
+} GraphType;
+
+// 그래프 초기화 
+void graph_init(GraphType* g)
+{
+	g->n = 0;
+	for (int i = 0; i < 2 * MAX_VERTICES; i++) {
+		g->edges[i].start = 0;
+		g->edges[i].end = 0;
+		g->edges[i].weight = INF;
 	}
+}
+// 간선 삽입 연산
+void insert_edge(GraphType* g, int start, int end, int w)
+{
+	g->edges[g->n].start = start;
+	g->edges[g->n].end = end;
+	g->edges[g->n].weight = w;
 	g->n++;
 }
-
-void insert_edge(GraphType* g, int start, int end)
+// qsort()에 사용되는 함수
+int compare(const void* a, const void* b)
 {
-	if (start >= g->n || end >= g->n) {
-		fprintf(stderr, "그래프: 정점 번호 오류");
-		return;
+	struct Edge* x = (struct Edge*)a;
+	struct Edge* y = (struct Edge*)b;
+	return (x->weight - y->weight);
+}
+
+// kruskal의 최소 비용 신장 트리 프로그램
+void kruskal(GraphType* g)
+{
+	int edge_accepted = 0;	// 현재까지 선택된 간선의 수	
+	int uset, vset;			// 정점 u와 정점 v의 집합 번호
+	struct Edge e;
+
+	set_init(g->n);				// 집합 초기화
+	qsort(g->edges, g->n, sizeof(struct Edge), compare);
+
+	printf("크루스칼 최소 신장 트리 알고리즘 \n");
+	int i = 0;
+	while (edge_accepted < (g->n - 1))	// 간선의 수 < (n-1)
+	{
+		e = g->edges[i];
+		uset = set_find(e.start);		// 정점 u의 집합 번호 
+		vset = set_find(e.end);		// 정점 v의 집합 번호
+		if (uset != vset) {			// 서로 속한 집합이 다르면
+			printf("간선 (%d,%d) %d 선택\n", e.start, e.end, e.weight);
+			edge_accepted++;
+			set_union(uset, vset);	// 두개의 집합을 합친다.
+		}
+		i++;
 	}
-	g->adj_mat[start][end] = 1;
-	g->adj_mat[end][start] = 1;
 }
-
-// 인접 행렬로 표현된 그래프에 대한 깊이 우선 탐색
-void dfs_mat(GraphType* g, int v)
-{
-	int w;
-	visited[v] = TRUE;		// 정점 v의 방문 표시 
-	printf("정점 %d -> ", v);		// 방문한 정점 출력
-	for (w = 0; w < g->n; w++) 		// 인접 정점 탐색
-		if (g->adj_mat[v][w] && !visited[w])
-			dfs_mat(g, w);	//정점 w에서 DFS 새로 시작
-}
-
 
 int main(void)
 {
 	GraphType* g;
 	g = (GraphType*)malloc(sizeof(GraphType));
-	init(g);
-	for (int i = 0; i < 11; i++)
-		insert_vertex(g, i);
-	insert_edge(g, 0, 9);
-	insert_edge(g, 0, 6);
-	insert_edge(g, 0, 4);
-	insert_edge(g, 0, 2);
-	insert_edge(g, 0, 5);
-	insert_edge(g, 9, 8);
-	insert_edge(g, 8, 10);
-	insert_edge(g, 6, 8);
-	insert_edge(g, 6, 7);
-	insert_edge(g, 7, 10);
-	insert_edge(g, 6, 4);
-	insert_edge(g, 4, 7);
-	insert_edge(g, 2, 4);
-	insert_edge(g, 2, 3);
-	insert_edge(g, 3, 4);
-	insert_edge(g, 3, 5);
-	insert_edge(g, 5, 4);
-	insert_edge(g, 5, 1);
-	insert_edge(g, 4, 1);
-	insert_edge(g, 1, 7);
-	insert_edge(g, 1, 10);
+	graph_init(g);
 
-	printf("깊이 우선 탐색\n");
-	dfs_mat(g, 9);
-	printf("\n");
+	/*insert_edge(g, 1, 2, 3);
+	insert_edge(g, 1, 6, 11);
+	insert_edge(g, 1, 7, 12);
+	insert_edge(g, 2, 6, 7);
+	insert_edge(g, 2, 5, 1);
+	insert_edge(g, 2, 4, 4);
+	insert_edge(g, 2, 3, 5);
+	insert_edge(g, 2, 7, 8);
+	insert_edge(g, 3, 7, 6);
+	insert_edge(g, 3, 4, 2);
+	insert_edge(g, 3, 8, 5);
+	insert_edge(g, 4, 5, 13);
+	insert_edge(g, 4, 8, 14);
+	insert_edge(g, 4, 10, 16);
+	insert_edge(g, 5, 6, 9);
+	insert_edge(g, 5, 9, 18);
+	insert_edge(g, 5, 10, 10);
+	insert_edge(g, 7, 8, 13);
+	insert_edge(g, 8, 10, 15);
+	insert_edge(g, 9, 10, 10);*/
+	insert_edge(g, 0, 1, 29);
+	insert_edge(g, 1, 2, 16);
+	insert_edge(g, 2, 3, 12);
+	insert_edge(g, 3, 4, 22);
+	insert_edge(g, 4, 5, 27);
+	insert_edge(g, 5, 0, 10);
+	insert_edge(g, 6, 1, 15);
+	insert_edge(g, 6, 3, 18);
+	insert_edge(g, 6, 4, 25);
+
+
+	kruskal(g);
 	free(g);
 	return 0;
 }
