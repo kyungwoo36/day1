191e0c32e7ca4bd79088cca1f23049f69ce426ef
5671218@stu.kmu.ac.kr <5671218@stu.kmu.ac.kr>
2023-11-08 16:31:26 +0900

    3차

--- day1/binaryserch.c
+++ day1/binaryserch.c
@@ -32,14 +32,14 @@ void prim(GraphType* g, int s)
 {
 	int i, u, v;
 
-	for (u = 0; u < g->n; u++)
+	for (u = 0; u<g->n; u++)
 		distance[u] = INF;
 	distance[s] = 0;
 	for (i = 0; i < g->n; i++) {
 		u = get_min_vertex(g->n);
 		selected[u] = TRUE;
 		if (distance[u] == INF) return;
-		printf("정점 %d 추가\n", u);
+		printf("정점 %d 추가\n", u+1);
 		for (v = 0; v < g->n; v++)
 			if (g->weight[u][v] != INF)
 				if (!selected[v] && g->weight[u][v] < distance[v])
